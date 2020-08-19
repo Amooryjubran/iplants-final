9857dcb626896416114c51be547c8d4fee58b562
amooryjubran <omarsalehjubran@gmail.com>
2020-08-19 17:34:03 -0400

    navbar responsive

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import './App.css';
-import Navbar from './Components/Layouts/Navbar';
-import Home from './Components/Home/Home';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Layouts/Navbar";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
     <>
       <Navbar />
-        <Switch>
-          <Route path="/" components={Home}/>
-        </Switch>
+      <Switch>
+        <Route path="/" components={Home} />
+      </Switch>
     </>
   );
 }
--- src/Components/Layouts/Navbar.js
+++ src/Components/Layouts/Navbar.js
@@ -1,59 +1,89 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import styled from 'styled-components';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import styled from "styled-components";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 export default function Navbar() {
-    return (
-        <Nav>
-            <div className="logo">
-                <h1 className="logo__text">iPlants</h1>
-            </div>
-            <NavbarLinks>
-                <li><StyledLink to="/">iHome</StyledLink></li>
-                <li><StyledLink to='/blogs'>iBlogs</StyledLink></li>
-                <li><StyledLink to="/inspiration">inspiration</StyledLink></li>
-                <li><StyledLink to='/contact'>iContact</StyledLink></li>
-                <li><StyledLink to='/Basket'> <ShoppingBasketIcon /></StyledLink></li>
-            </NavbarLinks>
-        </Nav>
-    )
+  const [state, setState] = useState(false);
+
+  const handleClose = () => {
+    if (state === true) {
+      setState(!state);
+    }
+  };
+
+  return (
+    <div className="Navbar">
+      <Nav>
+        <div className="logo" onClick={handleClose}>
+          <h1 className="logo__text">iPlants</h1>
+        </div>
+        <div className="arrow" onClick={() => setState(!state)}>
+          <ArrowDropDownIcon style={{ marginTop: "30px" }} />
+        </div>
+      </Nav>
+
+      <NavbarLinks
+        onClick={handleClose}
+        className={state ? "newLinks links" : "links"}
+      >
+        <li>
+          <StyledLink to="/">iHome</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/blogs">iBlogs</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/inspiration">inspiration</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/contact">iContact</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/Basket">
+            {" "}
+            <ShoppingBasketIcon />
+          </StyledLink>
+        </li>
+      </NavbarLinks>
+    </div>
+  );
 }
 
 const Nav = styled.div`
-    display: flex;
-    justify-content: space-around;
-    .logo__text{
-        background-color: lightgreen;
-        color: white;
-        padding: 2px 30px;
-        font-weight: 100;
-    }
-
+  display: flex;
+  justify-content: space-between;
+  .logo__text {
+    background-color: lightgreen;
+    color: white;
+    padding: 2px 30px;
+    font-weight: 100;
+  }
 `;
 
 const NavbarLinks = styled.nav`
-    list-style: none;
-    display: flex;    
-    li{
-        margin: 30px 20px;
-        color: black;
-        &:hover{
-            opacity: 0.2;
-        }
+  list-style: none;
+
+  li {
+    margin: 30px 20px;
+    color: black;
+    &:hover {
+      opacity: 0.2;
     }
+  }
 `;
 
 const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
     text-decoration: none;
-    color: black;
-
-    &:focus, &:hover, &:visited, &:link, &:active {
-        text-decoration: none;
-    }
+  }
 `;
-
-
-
-
-
